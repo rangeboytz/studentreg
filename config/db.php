56540e6dc975b505e14aa49d7ed672861bb6ee9a
rangeboytz <stephanorange7@gmail.com>
2026-05-16 15:37:02 +0300

    fix database connection for railway

--- config/db.php
+++ config/db.php
@@ -1,16 +1,5 @@
-<?php
-// Kuchukua connection details kutoka environment variables za Railway
-$host = getenv('MYSQLHOST') ?: getenv('DB_HOST') ?:'localhost';
-$dbname = getenv('MYSQLDATABASE') ?: getenv('DB_HOST') ?:'student_system';
-$username = getenv('MYSQLUSER') ?:getenv('DB_HOST') ?: 'root';
-$password = getenv('MYSQLPASSWORD') ?: getenv('DB_HOST') ?:'';
-$port = getenv('MYSQLPORT') ?:3306;
-
-
-try {
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+<?php $con = mysqli_connect("localhost","root","","student_system") ;
+if(!$con){
+    die("Connection Failed".mysqli_connect_error());
 }
 ?>
